8b4b0bd727f7f782948f760181c453ce2daf4915
Lichao Huang <365422537@qq.com>
2014-07-07 21:33:52 +0100

    a

--- IGOPCANET/GetIGO.m
+++ IGOPCANET/GetIGO.m
@@ -1,22 +1,21 @@
 function fhy = GetIGO(img_in)
 
-% [gx,gy] = gradient(img_in);
-% 
-% fhy = atan(gy./(gx+eps));
-% 
-% %correction
-% cor = find(gy<0 & gx<0);
-% fhy(cor) = fhy(cor)+pi;
-% cor = find(gy>0 & gx<0);
-% fhy(cor) = fhy(cor)+pi;
-% %cor = find(fhy<0);
-% %fhy(cor) = fhy(cor)+pi*2;
-% 
-% fhy = exp(1i*fhy);
+[gx,gy] = gradient(img_in);
+
+fhy = atan(gy./(gx+eps));
+
+%correction
+cor = find(gy<0 & gx<0);
+fhy(cor) = fhy(cor)+pi;
+cor = find(gy>0 & gx<0);
+fhy(cor) = fhy(cor)+pi;
+%cor = find(fhy<0);
+%fhy(cor) = fhy(cor)+pi*2;
+fhy = exp(1i*fhy);
 
 
-[bx,by] = myEdge(img_in,1);
-ang = angle(bx + 1i*by);
-fhy = cos(ang) + 1i*sin(ang);
+% [bx,by] = myEdge(img_in,1);
+% ang = angle(bx + 1i*by);
+% fhy = cos(ang) + 1i*sin(ang);
 
 end
--- IGOPCANET/text.m
+++
@@ -1,3 +0,0 @@
- [bx, by] = myEdge(temp, 1); ang = angle(bx + 1i*by);
-    temp = cos(ang) + 1i*sin(ang);
-temp = temp(:);
